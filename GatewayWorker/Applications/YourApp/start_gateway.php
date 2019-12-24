9b9a0e2e95b5874dda1a746dd3b0f3c751f54a64
戴 <d@192.168.0.105>
2019-12-24 21:08:40 +0800

    心跳

--- GatewayWorker/Applications/YourApp/start_gateway.php
+++ GatewayWorker/Applications/YourApp/start_gateway.php
@@ -35,9 +35,11 @@ $gateway->startPort = 2900;
 $gateway->registerAddress = '127.0.0.1:1238';
 
 // 心跳间隔
-// $gateway->pingInterval = 10;
+$gateway->pingInterval = 55;
+//
+$gateway->pingNotResponseLimit = 1;
 // 心跳数据
-// $gateway->pingData = '{"type":"pings"}';
+$gateway->pingData = '';
 
 
 // 当客户端连接上来时，设置连接的onWebSocketConnect，即在websocket握手时的回调
